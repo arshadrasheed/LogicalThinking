91223163544305cc57fe6fefffa62af18d2fdd53
ArshadKather <mohamed.arshad@ncircletech.com>
2024-05-31 16:35:14 +0530

    - Namespace Changed

--- PallindromeNumber_Program/Program.cs
+++ PallindromeNumber_Program/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PallindromeNumber_Program
+namespace _04_PallindromeNumber_Program
 {
     internal class Program
     {

f2e194c86935ce4bc51a59a1b59f189b8bc9263c
ArshadKather <mohamed.arshad@ncircletech.com>
2024-05-31 16:33:39 +0530

    - Pallindrome Number Completed

---
+++ PallindromeNumber_Program/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PallindromeNumber_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Number: ");
+            string number = Console.ReadLine();
+
+            string reverseNumber = string.Empty;
+
+            for (int i = number.Length; i > 0; i--)
+            {
+                reverseNumber = $"{reverseNumber}{number[i-1]}";
+            }
+
+            if (reverseNumber == number)
+            {
+                Console.WriteLine( $"The Entered Number {number} is Pallindrome Number");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

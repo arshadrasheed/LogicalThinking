154d543be22b0bf454fe39c52926ea5440627cba
ArshadKather <mohamed.arshad@ncircletech.com>
2024-05-31 16:45:02 +0530

    -Reverse Number Completed

---
+++ 05_ReverseNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_ReverseNumber_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int reverse = 0;
+
+            while (number > 0)
+            {
+                int remainder= number % 10;
+
+                reverse = (reverse * 10) + remainder;
+
+                number= number/ 10;
+            }
+
+            Console.Write(reverse);
+            Console.ReadKey();
+
+        }
+    }
+}
